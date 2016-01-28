0b0822c2e583447c874d9905d170d8609c4bc018
Md Faisal <fsl4faisal@gmail.com>
2016-01-28 15:37:52 +0530

    deleted servlets and added content according to spring framwork still working on it not functioning right now

--- src/main/java/com/marakana/contacts/controllers/ContactController.java
+++ src/main/java/com/marakana/contacts/controllers/ContactController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.marakana.contacts.entities.Address;
 import com.marakana.contacts.entities.Contact;
 import com.marakana.contacts.repositories.ContactRepository;
 
@@ -36,19 +36,79 @@ public class ContactController {
 	@RequestMapping(value = "/contact", method = RequestMethod.GET)
 	public void getContact(HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ServletException {
-		if (request.getParameter("add")!=null){
-			request.getRequestDispatcher("view/contact/add.jsp").forward(request,response);
-		}else{
-			///get contact id from request parameter and populate model
-			//with the contact and address objects
-			long id=Long.parseLong(request.getParameter("id"));
-			Contact contact=contactRepository.find(id);//TODO
+		if (request.getParameter("add") != null) {
+			request.getRequestDispatcher("view/contact/add.jsp").forward(
+					request, response);
+		} else {
+			// /get contact id from request parameter and populate model
+			// with the contact and address objects
+			long id = Long.parseLong(request.getParameter("id"));
+			Contact contact = contactRepository.findOne(id);// TODO
 			request.setAttribute("contact", contact);
-			
-			//dispatch either to the edit page or to view page
-			if(request.getParameter("edit")!=null){
-				request.getRequestDispatcher("view/contact.edit.jsp").forward(request, response);
+
+			// dispatch either to the edit page or to view page
+			if (request.getParameter("edit") != null) {
+				request.getRequestDispatcher("view/contact/edit.jsp").forward(
+						request, response);
 			}
+			else{
+				request.getRequestDispatcher("view/contact/view.jsp").forward(
+						request, response);
+			}
+		}
+
+	}
+	@RequestMapping(value = "/contact", method = RequestMethod.POST)
+	public void postContact(HttpServletRequest request,
+			HttpServletResponse response) throws IOException{
+		if (request.getParameter("add") != null) {
+			// create new contact and address from form parameters
+			// and persist
+			Address address = new Address(request.getParameter("street"),
+					request.getParameter("city"),
+					request.getParameter("state"), request.getParameter("zip"));
+			Contact contact = new Contact(request.getParameter("name"), address);
+
+			contact = contactRepository.save(contact);
+
+			// redirect to contact view page
+			 response.sendRedirect("contact?id=" + contact.getId());
+		} else if (request.getParameter("edit") != null) {
+			// lookup exiting contact and address, edit fields and persist
+			System.out.println("Inside ContectServlet doPost Edit");
+			long id = Long.parseLong(request.getParameter("id"));
+			Contact contact = contactRepository.findOne(id);
+			Address address = contact.getAddress();
+			contact.setName(request.getParameter("name"));
+			address.setCity(request.getParameter("city"));
+			address.setState(request.getParameter("state"));
+			address.setStreet(request.getParameter("street"));
+			address.setZip(request.getParameter("zip"));
+			contact.setAddress(address);
+			contact = contactRepository.save(contact);
+			System.out.println("details from contactServlet after the update :"
+					+ contact.getName());
+			// redirect to view page
+
+			System.out
+					.println("ContactServlet in edit doPost after saving contacts in the database");
+			for (Contact c : contactRepository.findAll()) {
+				System.out
+						.println("ContactServlet in edit doPost after saving contacts in the database:findAll() from ContactServlet :"
+								+ c.getName());
+			}
+
+			response.sendRedirect("contact?id=" + contact.getId());
+
+		} else if (request.getParameter("delete") != null) {
+			System.out.println("Inside delete doPost");
+			// delete and persist
+			long id = Long.parseLong(request.getParameter("id"));
+			Contact contact = contactRepository.findOne(id);
+			contactRepository.delete(contact);
+			// redirect to view page
+			response.sendRedirect("contacts");
+
 		}
 
 	}
--- src/main/java/com/marakana/contacts/repositories/ContactRepository.java
+++ src/main/java/com/marakana/contacts/repositories/ContactRepository.java
@@ -1,32 +1,10 @@
 package com.marakana.contacts.repositories;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.marakana.contacts.entities.Contact;
 
-public class ContactRepository extends Repository<Contact> {
+public interface ContactRepository extends JpaRepository<Contact, Long> {
 
-	private final EntityManager em = Persistence.createEntityManagerFactory(
-			"training2").createEntityManager();
-	
-	public ContactRepository() {
-		super(Contact.class);
-	}
-	
-	@Override
-	public List<Contact> findAll() {
-		List<Contact> contacts;
-		contacts=em.createQuery("from " + Contact.class.getSimpleName(),
-				Contact.class).getResultList();
-		for (Contact contact : contacts) {
-			System.out.println("inside findAll() function from ContactRepository.java :" + contact.getName());
-		}
-		System.out.println("inside findAll() function from ContactRepository.java size:"+contacts.size());
-		
-		return contacts;
-	}
 
 }
