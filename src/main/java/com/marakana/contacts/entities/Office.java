989e1f7b7ce8b9e478b9b949e81ccca0feef3217
Md Faisal <fsl4faisal@gmail.com>
2016-01-31 02:22:11 +0530

    updated Company and Office , so that there would be no need of join table Company_Office

--- src/main/java/com/marakana/contacts/entities/Office.java
+++ src/main/java/com/marakana/contacts/entities/Office.java
@@ -2,6 +2,7 @@ package com.marakana.contacts.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -13,6 +14,17 @@ public class Office extends BaseEntity{
 	@Column
 	private String name;
 	
+	@ManyToOne
+	private Company company;
+	
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+
 	public Office(){}
 
 	public Office(String name) {
